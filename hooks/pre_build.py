a34e105a72c720b00d67db727cdd67c74125164c
Vinay Ramasesh <ramasesh@google.com>
2020-10-10 12:58:43 -0700

    added exit code to git cleanliness hook

--- hooks/pre_build.py
+++ hooks/pre_build.py
@@ -1,22 +1,30 @@
 """Pre-build hook that checks that the repo is in an up-to-date state,
 and stores the commit hash in a file"""
 
-import os
+import os, sys
 from git import Repo
-import json
 
-COMMIT_HASH_FILE = ".container_hashes.json"
 
 def hook():
-  repo = Repo('..')
+  print('Running git-cleanliness hook')
+
+  repo = Repo('.')
+
+  EXIT_CODE = 0
 
   # check repo is clean
-  assert len(repo.untracked_files) == 0
-  assert not repo.is_dirty()
+  if len(repo.untracked_files) != 0:
+    print('Untracked files found!')
+    EXIT_CODE = 1
+  if repo.is_dirty():
+    print('Repo is dirty!')
+    EXIT_CODE = 1
+  if EXIT_CODE == 1:
+    sys.exit(EXIT_CODE)
 
   # get commit hash
   commit_hash = repo.commit().hexsha
-  
+  print(f"Latest commit: {commit_hash}")
 
-  if not os.path.exists(COMMIT_HASH_FILE):
-    json.dum
+if __name__=='__main__':
+  hook()
