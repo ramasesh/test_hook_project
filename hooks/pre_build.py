6510a718f8abf46502e813bcf4a73b4b70a80c72
Vinay Ramasesh <ramasesh@google.com>
2020-10-11 15:03:47 -0700

    great commit message

--- hooks/pre_build.py
+++ hooks/pre_build.py
@@ -4,9 +4,11 @@ and stores the commit hash in a file"""
 
 import os, sys
 from git import Repo
+from absl import logging
 
 def hook():
   print('Running git-cleanliness hook')
+  print('Running git-cleanliness hook 2')
 
   repo = Repo('.')
 
@@ -14,17 +16,17 @@ def hook():
 
   # check repo is clean
   if len(repo.untracked_files) != 0:
-    print('Untracked files found!')
+    logging.error('Untracked files found!')
     EXIT_CODE = 1
   if repo.is_dirty():
-    print('Repo is dirty!')
+    logging.error('Repo is dirty!')
     EXIT_CODE = 1
   if EXIT_CODE == 1:
     sys.exit(EXIT_CODE)
 
   # get commit hash
   commit_hash = repo.commit().hexsha
-  print(f"Latest commit: {commit_hash}")
+  print("{'commit': %s}" % commmit_hash)
 
 if __name__=='__main__':
   hook()
