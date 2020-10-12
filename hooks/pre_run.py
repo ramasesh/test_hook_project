06f54eb73a3e6a1ba414ac8f204cbdb02edcbbca
Vinay Ramasesh <ramasesh@google.com>
2020-10-12 16:17:46 -0700

    hi

--- hooks/pre_run.py
+++ hooks/pre_run.py
@@ -9,6 +9,7 @@ flags.DEFINE_string('container_id', None, 'ID of Docker container to use')
 def main(argv):
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
+  print("HERE!")
 
   FILE = '.container_info.json'
   with open(FILE, 'r') as f:
