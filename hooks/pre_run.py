091207f1e8bc8bffacdab4da7489c36572679821
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-10-13 03:38:49 +0000

    Removed container tracking file and processed commit hash directly from image label.

--- hooks/pre_run.py
+++ hooks/pre_run.py
@@ -2,18 +2,29 @@
 from absl import app
 from absl import flags
 import json
+import docker
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('container_id', None, 'ID of Docker container to use')
+
+
+def get_hash_from_image(container_id):
+  """Load git hash from docker image label."""
+  client = docker.from_env()
+  labels = client.images.get(container_id).labels
+  try:
+    return labels['commit']
+  except KeyError as e:
+    logging.error(f'Docker image {container_id} does not contain a git commit label.')
+    raise e
+     
 
 def main(argv):
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
 
-  FILE = '.container_info.json'
-  with open(FILE, 'r') as f:
-    data = json.load(f)
-  commit_hash = data[FLAGS.container_id]['commit']
+  commit_hash = get_hash_from_image(FLAGS.container_id)
+  
   print('{"commit": "%s"}' % commit_hash)
 
 if __name__ == '__main__':
